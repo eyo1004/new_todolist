10314ca24121c916d55f670bc860b9d1f83b8eb0
eyo1004 <1004333@gmail.com>
2024-09-25 10:43:38 +0900

    first todolist

---
+++ src/components/TodoList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './TodoList.scss';
+import TodoListItem from './TodoListItem';
+
+const TodoList = ({ todos, onRemove, onToggle }) => {
+  return (
+    <div className='Todolist'>
+      {
+        todos.map(todo => (
+          <TodoListItem 
+            todo={todo} 
+            key={todo.id} 
+            onRemove={onRemove} 
+            onToggle={onToggle} 
+          />
+        ))
+      }
+    </div>
+  )
+}
+
+export default TodoList;
